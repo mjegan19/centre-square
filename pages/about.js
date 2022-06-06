569ad305f07d775d18730f7e7382db971799a361
mjegan19 <87794144+mjegan19@users.noreply.github.com>
2022-06-06 22:34:54 +1000

    Updated API Call

--- pages/about.js
+++ pages/about.js
@@ -5,6 +5,7 @@ function AboutUs() {
     <div>
       <ScoreBoard pageInfo={'about'} title={'About Us'} description={'Find out everything you wanted to know about CentreSquare...'} />
 
+      <h2>About CentreSquare</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt mollitia at natus id quibusdam optio illo error magnam ullam, voluptatem necessitatibus libero amet quos similique!</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis sed quaerat magni assumenda qui, eaque minima beatae quidem iure praesentium quae in incidunt ipsam iusto rem dignissimos saepe cum aut atque enim distinctio facere fugiat. Distinctio quas aliquam eius nulla temporibus praesentium corrupti sequi? Iste.</p>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, pariatur labore voluptatem, saepe quo laudantium asperiores illum odio eligendi nobis voluptates earum voluptatum cum. Iste a cum alias deleniti repellendus dolor provident, impedit ratione optio!</p>
--- pages/contact.js
+++ pages/contact.js
@@ -5,6 +5,7 @@ function ContactUs() {
     <div>
       <ScoreBoard pageInfo={'contact'} title={'Contact Us'} description={'Reach out to us via our channels below'} />
 
+      <h2>Contact CentreSquare</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt mollitia at natus id quibusdam optio illo error magnam ullam, voluptatem necessitatibus libero amet quos similique!</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis sed quaerat magni assumenda qui, eaque minima beatae quidem iure praesentium quae in incidunt ipsam iusto rem dignissimos saepe cum aut atque enim distinctio facere fugiat. Distinctio quas aliquam eius nulla temporibus praesentium corrupti sequi? Iste.</p>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, pariatur labore voluptatem, saepe quo laudantium asperiores illum odio eligendi nobis voluptates earum voluptatum cum. Iste a cum alias deleniti repellendus dolor provident, impedit ratione optio!</p>
