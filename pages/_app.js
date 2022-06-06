9955618df2770e2361f10148baeeb9dde9d6da83
mjegan19 <87794144+mjegan19@users.noreply.github.com>
2022-06-06 11:06:58 +1000

    Integrated GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,27 @@
 import '../styles/sass/globals.scss';
 import Layout from '../components/layout/Layout';
+import Script from 'next/script';
 import { Fragment } from 'react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
+
+      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} strategy='afterInteractive' />
+
+      <Script
+        id="google-analytics-script"
+        strategy="afterInteractive"
+      >
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+        `}
+      </Script>
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
